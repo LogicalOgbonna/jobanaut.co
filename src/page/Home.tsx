4fc410352b595926c7d17b83c982a0555b81b49a
Jamesalehandro <jamesalehandro@gmail.com>
2023-03-12 13:41:07 +0000

    Updated

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -9,51 +9,47 @@ import { globalStyles } from '../styles';
 
 export const Home = () => {
   return (
-    <ColumnFlex w={globalStyles.fullWidth} flex='1'>
+    <ColumnFlex w={globalStyles.fullWidth} flex='1' gap='80px' my='80px'>
       <Unisection
-        showBtn={true}
-        title={'Discover the power of effortless job hunting with Jobanaut.'}
-        desc={
-          'Welcome to Jobanaut, the easiest way to find your dream job. With our powerful search engine and custom alerts, you will never miss a great opportunity.'
-        }
+        showBtn
+        title='Discover the power of effortless job hunting with Jobanaut.'
+        desc='Welcome to Jobanaut, the easiest way to find your dream job. With our powerful search engine and custom alerts, you will never miss a great opportunity.'
         addBg
         image={hero}
+        showGetBtn
       />
 
       <Unisection
-        title={'Create personalized Job Alerts'}
-        desc={
-          "Custom alerts can help you get better jobs by ensuring that you're the first to know about new opportunities that match your preferences. Instead of manually checking job sites and company career pages every day, you can set up job alerts on Jobanaut based on your preferred search criteria"
-        }
+        title='Create personalized Job Alerts'
+        desc="Custom alerts can help you get better jobs by ensuring that you're the first to know about new opportunities that match your preferences. Instead of manually checking job sites and company career pages every day, you can set up job alerts on Jobanaut based on your preferred search criteria"
         image={create}
       />
 
       <Unisection
-        title={'AI Powered Resume Builder'}
-        desc={
-          'With our AI-powered resume writing service, you can create a professional resume quickly and easily. Our AI technology analyzes your skills, experience, and qualifications, and generates a customized resume that is tailored to your specific career goals and job search needs. By using our AI-powered resume writing service, you can ensure that your resume stands out from the competition and impresses potential employers.'
-        }
+        title='AI Powered Resume Builder'
+        desc='With our AI-powered resume writing service, you can create a professional resume quickly and easily. Our AI technology analyzes your skills, experience, and qualifications, and generates a customized resume that is tailored to your specific career goals and job search needs. By using our AI-powered resume writing service, you can ensure that your resume stands out from the competition and impresses potential employers.'
         image={resume}
+        isReverse
       />
+
       <Unisection
-        title={'AI Powered Cover Letter Writer'}
-        desc={
-          'Our AI-powered cover letter writing service generates customized cover letters that highlight your strengths and demonstrate your fit for the job, helping you save time and increase your chances of landing an interview.'
-        }
+        title='AI Powered Cover Letter Writer'
+        desc='Our AI-powered cover letter writing service generates customized cover letters that highlight your strengths and demonstrate your fit for the job, helping you save time and increase your chances of landing an interview.'
         image={cover}
       />
+
       <Unisection
-        title={'Career Development Resources'}
-        desc={
-          'Jobanaut is the ultimate job hunting tool that harnesses the power of technology to streamline your job search and application. '
-        }
+        title='Career Development Resources'
+        desc='Jobanaut is the ultimate job hunting tool that harnesses the power of technology to streamline your job search and application.'
         image={career}
+        isReverse
       />
+
       <Unisection
-        title={'Career Development Resources'}
-        showGetBtn={true}
-        image={jobwaiting}
+        showBtn
+        title='Your Perfect Job is waiting for you'
         addBg
+        image={jobwaiting}
       />
     </ColumnFlex>
   );
